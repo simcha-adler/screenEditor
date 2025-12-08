970da619f3622878cb2980e5d3f629f7e014c64e
simcha-adler <ad882288@gmail.com>
2025-12-08 22:07:57 +0200

    תוספת משתני הגדרות לקוח, ותיקוני קוד קטנים

--- data_center.js
+++ data_center.js
@@ -12,3 +12,14 @@ let theStyles = null;
 let thePanel = null;
 let openedMenu = null;
 let styleState = {};
+
+
+/*======*/
+
+let userSettings = {
+    theme: 'light',       // 'light' או 'dark' (ממשק העורך)
+    showOutlines: false,  // האם להציג גבולות לכל האלמנטים בדף (לעזרה בעיצוב)
+    autoSave: true,       // האם לשמור אוטומטית
+    language: 'he',       // שפת ממשק
+    uiScale: 100          // גודל ממשק (זום)
+};
